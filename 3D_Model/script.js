7ba03be83690bb2784ec7450253747d28b47c6b1
Juncheng112 <juncheng006@gmail.com>
2025-01-31 10:45:48 +0800

    Update script.js

--- 3D_Model/script.js
+++ 3D_Model/script.js
@@ -5,7 +5,7 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 // Set background color to white
-renderer.setClearColor(0x10171C, 1); // Set background to blue
+renderer.setClearColor(0xFFFFFF, 1); // Set background to blue
 
 document.body.appendChild(renderer.domElement);
 
